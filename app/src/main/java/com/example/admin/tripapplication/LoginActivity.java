01c73765ef571c7a4b18436a39e2ab24501e0f63
bob price <bob0130@sbcglobal.net>
2017-09-12 21:47:19 -0400

    Initial commit

---
+++ app/src/main/java/com/example/admin/tripapplication/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.admin.tripapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    //TODO facebook authentication
+
+    //TODO google authentication
+
+    //TODO permissions check (internet, location)
+
+    //TODO regular authentication
+}
